ef83b3665766431db50449789b26bdba1ddf895f
mariuszf161 <48321017+mariuszf161@users.noreply.github.com>
2020-03-13 12:00:15 +0100

    1.0.0 Initial App

--- App.tsx
+++ App.tsx
@@ -1,12 +1,43 @@
-import React from 'react';
+import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>ToDo Application</Text>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="List" component={ToDoList} />
+        <Stack.Screen name="About" component={About} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>This is home page</Text>
     </View>
   );
+}
+function ToDoList(){
+  return (
+    <View style={{flex: 2, alignItems: 'flex-start', justifyContent: 'center'}}>
+      <Text>This is a works page </Text>
+    </View>
+  );
+}
+
+function About(){
+  return (
+    <View style={{flex: 1, alignItems: 'flex-end', justifyContent: 'center'}}>
+      <Text>This page is about me </Text>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -16,5 +47,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     fontSize: 10,
+    flexDirection: 'row',
+    height: 100,
+    padding: 20,
   },
 });
